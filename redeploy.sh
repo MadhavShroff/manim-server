8256565c54ca1e10440b9c949479ff78114e5e73
MadhavShroff <madhavshroff99@gmail.com>
2024-01-17 03:28:33 -0500

    Modified redeploy script

--- redeploy.sh
+++ redeploy.sh
@@ -1,5 +1,5 @@
-homeDir="/home/ubuntu/makeitaifor-me-server";
+homeDir="/home/ubuntu/manim-server";
 
 cd $homeDir; # go to home directory
 
-/home/ubuntu/.nvm/versions/node/v18.16.1/bin/pm2 startOrRestart $homeDir/ecosystem.config.js --name "ai-server"; # start server
+/home/ubuntu/.nvm/versions/node/v18.16.1/bin/pm2 startOrRestart $homeDir/ecosystem.config.js --name "manim-server"; # start server
